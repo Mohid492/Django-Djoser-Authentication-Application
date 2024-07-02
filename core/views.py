f26dcd9a3e25bcce254a3b4bb71680cac8f54812
Mohid <mohidcaptainprice@gmail.com>
2024-07-02 21:40:23 +0500

    Removed unused imports

--- core/views.py
+++ core/views.py
@@ -1,27 +1,16 @@
-from django.forms import ValidationError
 from django.http import JsonResponse
-from django.contrib.auth import authenticate
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
 from django.contrib.auth import get_user_model
-from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes  # Add this line
-from .serializers import PasswordResetCodeSerializer, UserSerializer, VerifyPasswordResetCodeSerializer
 from django.core.mail import send_mail
-from rest_framework.exceptions import AuthenticationFailed
 from django.conf import settings
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from .models import *
-from django.utils.encoding import force_str
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
 
+from .serializers import PasswordResetCodeSerializer, VerifyPasswordResetCodeSerializer
 User = get_user_model()
 
 def get_reset_data(request):
